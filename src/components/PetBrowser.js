43c3bab5c878abe89a61946506588eb2ac4adc95
Cole Flournoy <flournoy.cole@gmail.com>
2021-05-19 18:18:41 -0400

    commit tree 42e1c2e7a99d852eb4fa0222d24e6613a8f11219

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -6,6 +6,13 @@ class PetBrowser extends React.Component {
   render() {
     return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
   }
+
+
+  render() {
+    const petComps = this.props.pets.map(p => <Pet pet={p} key={p.id} onAdoptPet={this.props.onAdoptPet} />)
+    return <div className="ui cards">{petComps}</div>
+  }
 }
+
 
 export default PetBrowser
